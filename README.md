86a82b30c26a5284598447f2737238a306f5115b
Miaozhen Zhang <miaozhenzhang666@gmail.com>
2019-11-29 15:29:37 -0600

    Complete the exercises in bare_minimum/promiseConstructor.js and make the Promise constructor tests pass

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Setup
-1. Run `npm install` to install all the dependencies
+1. Run `npm install` to install all the dependencies.
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,13 +9,41 @@ var request = require('request');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = function (filePath) {
+  // Create a new promise. The passed in function will receive functions 'resolve' and 'reject' as its arguments which can be called to seal the fate of the created promise.
+  // Reference: http://bluebirdjs.com/docs/api/new-promise.html
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, 'utf8', function (err, content) {
+      if (err) {
+        reject(err);
+      } else {
+        let firstLine = content.split('\n')[0];
+        resolve(firstLine);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function (resolve, reject) {
+    request.get(url, function (err, response) {
+      // This regular if-else pattern works
+
+      // if (err) {
+      //   reject(err);
+      // } else {
+      //   resolve(response.statusCode);
+      // }
+
+      // An alternate way to implement our error guard
+      if (err) {
+        reject(err);
+      }
+
+      resolve(response.statusCode);
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
